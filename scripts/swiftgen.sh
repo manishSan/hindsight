39d1c5a51635b4b4647a3b641dc94919395cf8a2
Sanwal, Manish <manish.sanwal@chase.com>
2018-11-13 15:56:15 -0500

    Updated swiftgen to refer the .yml file.

--- scripts/swiftgen.sh
+++ scripts/swiftgen.sh
@@ -1,8 +1,9 @@
-"$PODS_ROOT/SwiftGen/bin/swiftgen" storyboards "$PROJECT_DIR" --template "swift4" --output "$PROJECT_DIR/hindsight/Resources/Generated.Storyboards.swift"
-"$PODS_ROOT/SwiftGen/bin/swiftgen" images "$PROJECT_DIR" --template "swift4" --output "$PROJECT_DIR/hindsight/Resources/Generated.Images.swift" --enumName "ImageAsset"
-"$PODS_ROOT/SwiftGen/bin/swiftgen" strings "$PROJECT_DIR/hindsight/Resources/en.lproj/Localizable.strings" --param enumName="Constants" --template "structured-swift4" --output  "$PROJECT_DIR/hindsight/Resources/Generated.Strings.swift"
-#"$PODS_ROOT/SwiftGen/bin/swiftgen" fonts "$PROJECT_DIR/ProlificLibrary/Resources/Fonts" --template "swift3" --output "$PROJECT_DIR/DMM/Resources/Generated.Fonts.swift"
-"$PODS_ROOT/SwiftGen/bin/swiftgen" colors "$PROJECT_DIR/hindsight/Resources/colors.txt" --template "swift4" --output "$PROJECT_DIR/hindsight/Resources/Generated.Colors.swift"
+"$PODS_ROOT/SwiftGen/bin/swiftgen"
+#"$PODS_ROOT/SwiftGen/bin/swiftgen" storyboards "$PROJECT_DIR" --template "swift4" --output "$PROJECT_DIR/hindsight/Resources/Generated.Storyboards.swift"
+#"$PODS_ROOT/SwiftGen/bin/swiftgen" images "$PROJECT_DIR" --template "swift4" --output "$PROJECT_DIR/hindsight/Resources/Generated.Images.swift" --enumName "ImageAsset"
+#"$PODS_ROOT/SwiftGen/bin/swiftgen" strings "$PROJECT_DIR/hindsight/Resources/en.lproj/Localizable.strings" --param enumName="Constants" --template "structured-swift4" --output  "$PROJECT_DIR/hindsight/Resources/Generated.Strings.swift"
+##"$PODS_ROOT/SwiftGen/bin/swiftgen" fonts "$PROJECT_DIR/ProlificLibrary/Resources/Fonts" --template "swift3" --output "$PROJECT_DIR/DMM/Resources/Generated.Fonts.swift"
+#"$PODS_ROOT/SwiftGen/bin/swiftgen" colors "$PROJECT_DIR/hindsight/Resources/colors.txt" --template "swift4" --output "$PROJECT_DIR/hindsight/Resources/Generated.Colors.swift"
 
 
 
